8d6ba43c82a0b72833943db738382624dc612178
fejda <frankerikjoel290307@gmail.com>
2026-05-26 12:09:45 -0500

    fix: add @ComponentScan to register controllers

--- src/main/java/dev/fejda/latinoamericacomparte/EcuadorSharesApplication.java
+++ src/main/java/dev/fejda/latinoamericacomparte/EcuadorSharesApplication.java
@@ -2,10 +2,11 @@ package dev.fejda.latinoamericacomparte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "dev.fejda.latinoamericacomparte")  // ← AGREGA ESTA LÍNEA
 public class EcuadorSharesApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EcuadorSharesApplication.class, args);
     }
